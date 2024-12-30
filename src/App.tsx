d0319de96f2997521eac40fd8027fefe40b1da60
steveflevc <steveflevc@gmail.com>
2024-12-30 14:24:45 +0900

    feature: 탄스택 쿼리 세팅 및 devtools 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react';
-import { umbraApi } from '@/lib/umbraApi';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import MainPage from '@/app/page';
 
+const queryClient = new QueryClient();
+
 const App = () => {
-  useEffect(() => {
-    // tmdb api 연결 테스트
-    const testApi = async () => {
-      const response = await umbraApi.get('/movie/changes?page=1');
-    };
-
-    testApi();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* 개발환경에서만 노출 */}
+      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   );
 };
 
