5963a0ddd88fe8893e7808e2e71eafe130be0fe9
steveflevc <steveflevc@gmail.com>
2024-12-30 16:54:28 +0900

    feature: 기본 레이아웃 UI 수정

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 left-0 w-full h-12 py-3 px-10 border border-b bg-white">
-      <div>Umbra</div>
+    <header className="sticky top-0 left-0 w-full py-3 px-10 border-b bg-white z-50">
+      <div className="h-12  flex items-center">
+        <div>Umbra</div>
+      </div>
     </header>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/app/components/header';
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-col">
         <Outlet />
