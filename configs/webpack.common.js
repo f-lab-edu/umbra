2ae6675e61af11f7c2c923b1ad9de5efd8140af7
steveflevc <steveflevc@gmail.com>
2024-12-30 11:39:16 +0900

    feature: 이미지관련 설정 수정

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -27,11 +27,11 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: 'asset',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: 'asset',
       },
     ],
   },
