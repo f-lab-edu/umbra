c61c509f32a0ff104903761794fef2a41b97a41d
steveflevc <steveflevc@gmail.com>
2025-02-05 23:16:15 +0900

    style: 영화 상세 모달 스타일 수정

--- src/app/movie/components/movie-detail-content.tsx
+++ src/app/movie/components/movie-detail-content.tsx
@@ -6,14 +6,12 @@ const MovieDetailContent = ({ movieId }: { movieId: number }) => {
   const { data } = useGetMovieDetails({ id: movieId });
 
   return (
-    <div className="flex flex-col w-full h-full p-5">
-      <div className="flex w-full justify-center ">
-        <LazyImage
-          className="w-[500px] h-[500px]"
-          alt={'movie'}
-          src={`https://image.tmdb.org/t/p/w780/${data.posterPath}`}
-        />
-      </div>
+    <div className="flex flex-col w-full h-full justify-center items-center p-5">
+      <LazyImage
+        className="w-[300px] h-[400px]"
+        alt={'movie'}
+        src={`https://image.tmdb.org/t/p/w780/${data.posterPath}`}
+      />
       <div className="flex justify-center p-5 text-xl font-bold">{data.originalTitle}</div>
     </div>
   );
