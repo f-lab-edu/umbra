c7aa9fd41a9881edb3de50713ec22c4d367b5de6
steveflevc <steveflevc@gmail.com>
2025-02-19 00:13:58 +0900

    feature: webpack -> rsbuild 으로 변경

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,27 +1,15 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import { StorybookConfig } from 'storybook-react-rsbuild';
 
 const config: StorybookConfig = {
   stories: ['../src/stories/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
   ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: 'storybook-react-rsbuild',
     options: {},
   },
 };
