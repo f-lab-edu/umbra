a6f71f87659d3f6f92626c394a52a2ca1ec49f4f
steveflevc <steveflevc@gmail.com>
2025-04-08 21:49:47 +0900

    feat: 헤더 링크 추가

--- apps/umbra-nextjs/src/components/layout/header.tsx
+++ apps/umbra-nextjs/src/components/layout/header.tsx
@@ -9,6 +9,7 @@ const Header = () => {
         {/* TODO: 해당 컴포넌트를 무조건 클라이언트 사이드 렌더링을 해야 하는데 지금 Header 가 _app 에 레이아웃으로 구성되어 있어서,, 방법이 있을까요?.. */}
         {/* <NotificationBell /> */}
         <Link href="/bookmarks">북마크</Link>
+        <Link href="/movie-recommend">영화 추천</Link>
       </div>
     </header>
   );
