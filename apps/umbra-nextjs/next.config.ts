055db13dc8e8842730f25c6bcb5c7ab83a66aa36
steveflevc <steveflevc@gmail.com>
2025-05-01 14:24:11 +0900

    chore: ssg 배포 설정

--- apps/umbra-nextjs/next.config.ts
+++ apps/umbra-nextjs/next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [{ hostname: 'image.tmdb.org' }],
+    unoptimized: true,
   },
+  assetPrefix: '..',
+  trailingSlash: true,
 };
 
 export default nextConfig;
